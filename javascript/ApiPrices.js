cbc5739f16b63da9c991660dc59255888f342e46
gerpi <gerpi.2666@gmail.com>
2022-11-16 18:19:41 -0600

    include js file api UE agri

---
+++ javascript/ApiPrices.js
@@ -0,0 +1,26 @@
+let FR = document.querySelector('#fr')
+let PT= document.querySelector('#pt')
+let AL= document.querySelector('#de')
+
+function GetCountryBeff(code) {
+    fetch('https://ec.europa.eu/agrifood/api/beef/prices?memberStateCodes='+code+'&years=2019,2020&months=1,3,9&weeks=5,6,7,8,40,41,42&beginDate=01/09/2019&endDate=02/02/2020&carcassCategories=heifers,cows')
+        .then((res) => res.json())
+        .then((res) => {
+           let count=0;
+            let avg=0;
+           res.forEach(element => {
+                if(element.category=='Heifers'){
+                    let str=element.price.splice(1);
+                    avg+=parseFloat(str);
+                    count++;
+                }
+           });
+           console.log(avg)
+           console.log(count)
+           //avg=avg/count;
+
+            
+        })
+}
+
+
