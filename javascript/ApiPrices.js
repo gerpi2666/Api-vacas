9bfbdcde811699447faf4cf429d6ee375461578e
EliexesG <107147734+EliexesG@users.noreply.github.com>
2022-11-17 11:32:18 -0600

    Finalización API

--- javascript/ApiPrices.js
+++ javascript/ApiPrices.js
@@ -3,14 +3,14 @@ let PT = document.querySelector('#pt')
 let AL = document.querySelector('#de')
 
 FR.addEventListener('click', () => {
-    GetCards('FR', 636.54)
+    GetCards('FR', 636.54, 'Francia')
 })
 PT.addEventListener('click', () => {
-    GetCards('PT', 636.54)
+    GetCards('PT', 636.54, 'Portugal')
 })
 
 AL.addEventListener('click', () => {
-    GetCards('DE', 636.54)
+    GetCards('DE', 636.54, 'Alemania')
 })
 
 const formatter = new Intl.NumberFormat('es-CR', {
@@ -18,9 +18,13 @@ const formatter = new Intl.NumberFormat('es-CR', {
     currency: 'CRC'
 });
 
-async function GetCards(code, exchangeRate) {
+async function GetCards(code, exchangeRate, country) {
     
     let cartas = document.querySelector('#cartas');
+    let titulo = document.querySelector('#paisTitulo');
+    let loader = `<svg><circle cx="25" cy="25" r="25"></circle></svg>`;
+    cartas.innerHTML = loader;
+    titulo.innerHTML = "";
 
     let categorias = [{
         nombre: 'Novillas',
@@ -35,21 +39,22 @@ async function GetCards(code, exchangeRate) {
 
     let htmlCards = "";
     for (let i = 0; i <categorias.length; i++) {
-        let kilo;
         let precioPromedio = await getPrices(code, exchangeRate, categorias[i].nombreBusqueda)
-        kilo=precioPromedio;
+        let kilo=precioPromedio / 100;
+
                 htmlCards += `<div class="card p-0 col m-2" style="width: 18rem;">
                         <img src="${imgSrc(categorias[i].nombreBusqueda)}" class="card-img-top">
                         <div class="card-body">
                             <h5 class="card-title text-center">Precio por ${categorias[i].nombre}</h5>
                             
-                            <p class="text-center">Precio promedio canal<span class="mt-2"><br>${precioPromedio}</span></p>
-                            <p class="text-center">Precio promedio canal por kilo</p>
+                            <p class="text-center">Precio promedio canal<span class="mt-2"><br>${formatter.format(precioPromedio)}</span></p>
+                            <p class="text-center">Precio promedio canal por kilo<span class="mt-2"><br>${formatter.format(kilo)}</span></p>
 
                             </div>
                         </div>`
     }
 
+    titulo.innerHTML = country;
     cartas.innerHTML = htmlCards;
 
 }
@@ -72,7 +77,6 @@ async function getPrices(code, exchangeRate, category) {
     });
 
     avg = (avg / count) * exchangeRate;
-    avg = formatter.format(avg);
     return avg;
 }
 
